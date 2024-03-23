f1244e0686b722df5771f4e651d42917311b2442
lucarl07 <junitinho2007@gmail.com>
2024-03-23 12:45:07 -0300

    Adicionar código não funcional em ./exercicios/ex001

---
+++ exercicios/ex001/src/Main/Main.jsx
@@ -0,0 +1,49 @@
+const luana = {
+    cliente: 'Luana',
+    idade: 27,
+    compras: [
+        { nome: 'Notebook', preco: 'R$ 2500' },
+        { nome: 'Geladeira', preco: 'R$ 3000' },
+        { nome: 'Smartphone', preco: 'R$ 1500' },
+    ],
+    ativa: true,
+};
+
+const mario = {
+    cliente: 'Mario',
+    idade: 31,
+    compras: [
+        { nome: 'Notebook', preco: 'R$ 2500' },
+        { nome: 'Geladeira', preco: 'R$ 3000' },
+        { nome: 'Smartphone', preco: 'R$ 1500' },
+        { nome: 'Guitarra', preco: 'R$ 3500' },
+    ],
+    ativa: false,
+};
+
+const somarDespesas = (compras) => {
+    let acumulo;
+
+    compras.reduce((item, acc) => {
+        //...
+    })
+
+    return acumulo;
+}
+
+const App = () => {
+    const dados = luana;
+    const situacao = dados.ativa ? 'Ativa' : 'Inativa';
+
+    return (
+        <div>
+            <p>Nome: <span>{dados.cliente}</span></p>
+            <p>Idade: <span>{dados.idade}</span></p>
+            <p>Situação: <span>{situacao}</span></p>
+            <p>Gasto Total: <span>{somarDespesas(dados.compras)}</span></p>
+            <p>...</p>
+        </div>
+    );
+}
+
+export default App;
--- exercicios/ex001/src/script.jsx
+++ exercicios/ex001/src/script.jsx
@@ -1,8 +1,17 @@
+/** EXERCÍCIO 01 - VERIFICAR FINANÇAS
+ * Regras:
+ * 1. Mostre os dados da aplicação, como apresentado na aula
+ * 2. Não utilize CSS externo, use o style para mudar as cores
+ * 3. Se a situação estiver ativa pinte de verde, inativa vermelho
+ * 4. Se o gasto for maior que 10000 mostre uma mensagem
+ */
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './Main/Main'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
 )
