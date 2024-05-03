61808ba7058dda298f4779e7dc704184c3e9b767
lucarl07 <junitinho2007@gmail.com>
2024-05-03 11:44:40 -0300

    Utilizar hooks de formulário no React

---
+++ react-hooks/src/form/Input.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Input = () => {
+  const [nome, setNome] = React.useState("")
+
+  const clickSubmit = (event) => {
+    event.preventDefault()
+    console.log(event);
+  }
+
+  return (
+    <form onSubmit={clickSubmit}>
+      <h2>{nome}</h2>
+      <label htmlFor="inNome">Nome:</label>
+      <input 
+        type="text" 
+        id="inNome"
+        value={nome}
+        onChange={(event) => setNome(event.target.value)}
+      />
+      <input type="submit" value="Enviar" />
+    </form>
+  );
+}
+
+export default Input;
--- react-hooks/src/main.jsx
+++ react-hooks/src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './revisao_use_state/contador/App'
-import List from './revisao_use_state/lista_item/List'
+import Input from './form/Input'
+// import App from './revisao_use_state/contador/App'
+// import List from './revisao_use_state/lista_item/List'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    {/* <App/>
     <br />
     <List/>
+    <br /> */}
+    
+    <Input/>
   </React.StrictMode>,
 )
