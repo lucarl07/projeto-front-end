5a0d30d418c01e54486578ed2aa230ef04af0a74
lucarl07 <junitinho2007@gmail.com>
2024-05-10 11:38:15 -0300

    Primeiro teste com styled-components

--- framework-css/src/components/App/App.jsx
+++ framework-css/src/components/App/App.jsx
@@ -1,11 +1,30 @@
-import './App.css'
+import styled from "styled-components";
+
+const Cores = {
+  branco: '#fff',
+  preto: '#111',
+  amarelo: '#e1ce1b',
+  rosa: '#f2d',
+  verde: '#4aac46'
+}
+
+const Titulo = styled.h1 `color: ${Cores.verde};`
+
+const Titulo2 = styled.h2 `color: ${Cores.preto};`
+
+const Container = styled.div `
+  width: 400px;
+  height: 400px;
+  margin: 0 auto;
+  background-color: ${Cores.rosa};
+`;
 
 const App = () => {
   return (
-    <>
-      <h1>Hello World!</h1>
-      <h1>Olá mundo!</h1>
-    </>
+    <Container>
+      <Titulo>Hello world!</Titulo>
+      <Titulo2>Seja bem-vindo!</Titulo2>
+    </Container>
   );
 }
 
--- framework-css/src/main.jsx
+++ framework-css/src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Article from './components/Article/Article'
 import App from './components/App/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Article />
   </React.StrictMode>,
 )
